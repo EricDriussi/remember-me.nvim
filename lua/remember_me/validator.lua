e2b5348e576be80962bc11e955eaf371564a0093
Eric Driussi <ericdriussi@gmail.com>
2022-12-05 13:26:23 +0000

    refactor validator

--- lua/remember_me/validator.lua
+++ lua/remember_me/validator.lua
@@ -9,26 +9,20 @@ M.current_ft_against = function(ignored_ft)
   return true
 end
 
-local function get_parent_path(current)
-  return vim.fn.fnamemodify(current, ":h")
-end
-
-local function get_dir(path)
-  return string.match(path, ".*/(.*)$")
-end
-
 M.if_in_project = function(valid_roots)
   local current = vim.fn.getcwd()
   local parent_path = current
 
   while true do
     for _, root in ipairs(valid_roots) do
-      if vim.fn.globpath(parent_path, root) ~= "" then
-        return true, get_dir(parent_path)
+      local parent_is_root = vim.fn.globpath(parent_path, root) ~= ""
+      if parent_is_root then
+        local parent_dir = string.match(parent_path, ".*/(.*)$")
+        return true, parent_dir
       end
     end
 
-    current, parent_path = parent_path, get_parent_path(parent_path)
+    current, parent_path = parent_path, vim.fn.fnamemodify(parent_path, ":h")
     if parent_path == current then
       break
     end
